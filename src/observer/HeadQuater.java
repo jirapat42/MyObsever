92ce8cfaf88d5dc4fd4353602f3b7af62129a9ad
jirapat094 <60957643+jirapat42@users.noreply.github.com>
2020-08-26 21:35:29 +0700

    jirapat

--- src/observer/HeadQuater.java
+++ src/observer/HeadQuater.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package observer;
-
+import java.util.Observer;
 import java.util.ArrayList;
 import java.util.Observable;
 
@@ -11,30 +11,17 @@ import java.util.Observable;
  *
  * @author sarun
  */
-public class HeadQuater extends Observable  {
-    private final ArrayList<Observable> list;
+public class HeadQuater extends Observable {
+    
     private int someData;
 
-    public HeadQuater() {
-        this.list = new ArrayList<Observable>();
-    }
     public void setSomeData(int aData) {
-	someData = aData;
-        setChanged();
+    someData = aData;
+    setChanged();
 	notifyObservers();
     }
     public int getSomeData() {
 	return someData;
     }
-    
-    public void register (Observable observer) {
-        list.add(observer);
-    }
-     
-   public void notifyObservers() {
-        for (int j = 0; j < list.size(); ++j) {
-            list.get(j).update(this);
-	}
-    }
-    
+   
 }
